9cbdc6c4ce1dae5d4d00e6795f46b684e4ab6431
harmolipi <nikob381@gmail.com>
2022-03-14 16:38:38 -0400

    Fixed paths

--- .eleventy.js
+++ .eleventy.js
@@ -104,13 +104,13 @@ module.exports = function (eleventyConfig) {
     // Leading or trailing slashes are all normalized away, so don’t worry about it.
     // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
     // This is only used for URLs (it does not affect your file structure)
-    pathPrefix: '/src',
+    pathPrefix: '/',
 
     markdownTemplateEngine: 'liquid',
     htmlTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
     dir: {
-      input: './src',
+      input: 'src',
       includes: '_includes',
       data: '_data',
       output: '_site',
