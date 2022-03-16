a456f012ef61c0af5e7585bf52d551b16dafca75
harmolipi <nikob381@gmail.com>
2022-03-16 00:19:39 -0400

    Fixed content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: [
-    'src/_includes/**/*.njk',
-    'src/_includes/assets/css/*.css',
-    '_site/index.html',
+    './src/_includes/**/*.njk',
+    './src/_includes/assets/css/*.css',
+    './_site/index.html',
   ],
   theme: {
     extend: {},
